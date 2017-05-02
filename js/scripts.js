1f06981e3382455248c586c078858d0a46093780
Brian Rush and Corey Redding <brian+corey@>
2017-05-02 15:16:13 -0700

    add all html content, and all javascript logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
+//Business Logic
+
+var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+//User Interface Logic
+
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var vowels = ["a", "e", "i", "o", "u"]
-    var inputString = $("#string-in").val();
-    var inputSliced = inputString.slice("");
-    alert(inputSliced);
+
+    var letters = $("#string-in").val().split("");
+
+    for (index = 0; index < letters.length; index += 1) {
+      if (vowels.includes(letters[index])) {
+        letters[index] = "-";
+      }
+    }
+
+    letters = letters.join("");
+    $(".output").text(letters);
   });
 });
+    // var letters = [];
+    // letters.push(inputSliced);
+    // console.log(inputSliced);
+// });
